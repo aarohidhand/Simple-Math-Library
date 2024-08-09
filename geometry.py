5317601532ad40649ccc452e11b42cf4df21f50e
aarohidhand <150023944+aarohidhand@users.noreply.github.com>
2024-08-09 11:16:24 +0530

    Add files via upload

---
+++ geometry.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def area_of_rectangle(length, breadth):
+
+    '''
+    This function returns the area of rectangle
+    '''
+
+    return length*breadth
+
+def area_of_circle(radius):
+
+    '''
+    This function returns the area of circle
+    '''
+
+    return np.pi*(radius**2)
+
